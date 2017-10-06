f335f3799c75640e4241950b61d09cf5af57ec27
= <numenor21@gmail.com>
2017-10-06 08:40:54 -0400

    Combine styles and scripts via webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,15 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.scripts([
+	'resources/assets/js/jquery-3.2.1.slim.js',
+	'resources/assets/js/popper.min.js',
+	'resources/assets/js/bootstrap4.js',
+	'resources/assets/js/holder.js',
+	'resources/assets/js/app.js',
+], 'public/js/app.js');
+
+mix.styles([
+	'resources/assets/css/bootstrap.css',
+    'resources/assets/css/style.css',
+], 'public/css/app.css');
